cc5292abbcb8cae8184d7cb3e2f1cd672f7340b7
victor.velchev <victor.velchev@gmail.com>
2014-06-09 15:22:58 +0300

    added wall decoration

--- NinjaVsTrollsRefactored/scripts/dojo.js
+++ NinjaVsTrollsRefactored/scripts/dojo.js
@@ -10,6 +10,206 @@
     dojoWall.attr("stroke", "black");
     dojoWall.attr("stroke-width", 2);
 
+    var wallSeparateTwo = paper.rect(48, 0, 1, 549);
+    wallSeparateTwo.attr("fill", "SaddleBrown");
+    wallSeparateTwo.attr("stroke", "");
+    wallSeparateTwo.attr("stroke-width", 0);
+
+    var wallSeparateOne = paper.rect(50, 0, 1, 549);
+    wallSeparateOne.attr("fill", "Sienna");
+    wallSeparateOne.attr("stroke", "");
+    wallSeparateOne.attr("stroke-width", 0);
+
+    var wallSeparateTwoA = paper.rect(98, 0, 1, 549);
+    wallSeparateTwoA.attr("fill", "SaddleBrown");
+    wallSeparateTwoA.attr("stroke", "");
+    wallSeparateTwoA.attr("stroke-width", 0);
+
+    var wallSeparateOneA = paper.rect(100, 0, 1, 549);
+    wallSeparateOneA.attr("fill", "Sienna");
+    wallSeparateOneA.attr("stroke", "");
+    wallSeparateOneA.attr("stroke-width", 0);
+
+    var wallSeparateTwoB = paper.rect(148, 0, 1, 549);
+    wallSeparateTwoB.attr("fill", "SaddleBrown");
+    wallSeparateTwoB.attr("stroke", "");
+    wallSeparateTwoB.attr("stroke-width", 0);
+
+    var wallSeparateOneB = paper.rect(150, 0, 1, 549);
+    wallSeparateOneB.attr("fill", "Sienna");
+    wallSeparateOneB.attr("stroke", "");
+    wallSeparateOneB.attr("stroke-width", 0);
+
+    var wallSeparateTwoC = paper.rect(198, 0, 1, 549);
+    wallSeparateTwoC.attr("fill", "SaddleBrown");
+    wallSeparateTwoC.attr("stroke", "");
+    wallSeparateTwoC.attr("stroke-width", 0);
+
+    var wallSeparateOneC = paper.rect(200, 0, 1, 549);
+    wallSeparateOneC.attr("fill", "Sienna");
+    wallSeparateOneC.attr("stroke", "");
+    wallSeparateOneC.attr("stroke-width", 0);
+
+    var wallSeparateTwoD = paper.rect(248, 0, 1, 549);
+    wallSeparateTwoD.attr("fill", "SaddleBrown");
+    wallSeparateTwoD.attr("stroke", "");
+    wallSeparateTwoD.attr("stroke-width", 0);
+
+    var wallSeparateOneD = paper.rect(250, 0, 1, 549);
+    wallSeparateOneD.attr("fill", "Sienna");
+    wallSeparateOneD.attr("stroke", "");
+    wallSeparateOneD.attr("stroke-width", 0);
+
+    var wallSeparateTwoE = paper.rect(298, 0, 1, 549);
+    wallSeparateTwoE.attr("fill", "SaddleBrown");
+    wallSeparateTwoE.attr("stroke", "");
+    wallSeparateTwoE.attr("stroke-width", 0);
+
+    var wallSeparateOneE = paper.rect(300, 0, 1, 549);
+    wallSeparateOneE.attr("fill", "Sienna");
+    wallSeparateOneE.attr("stroke", "");
+    wallSeparateOneE.attr("stroke-width", 0);
+
+    var wallSeparateTwoF = paper.rect(348, 0, 1, 549);
+    wallSeparateTwoF.attr("fill", "SaddleBrown");
+    wallSeparateTwoF.attr("stroke", "");
+    wallSeparateTwoF.attr("stroke-width", 0);
+
+    var wallSeparateOneF = paper.rect(350, 0, 1, 549);
+    wallSeparateOneF.attr("fill", "Sienna");
+    wallSeparateOneF.attr("stroke", "");
+    wallSeparateOneF.attr("stroke-width", 0);
+
+    var wallSeparateTwoG = paper.rect(398, 0, 1, 549);
+    wallSeparateTwoG.attr("fill", "SaddleBrown");
+    wallSeparateTwoG.attr("stroke", "");
+    wallSeparateTwoG.attr("stroke-width", 0);
+
+    var wallSeparateOneG = paper.rect(400, 0, 1, 549);
+    wallSeparateOneG.attr("fill", "Sienna");
+    wallSeparateOneG.attr("stroke", "");
+    wallSeparateOneG.attr("stroke-width", 0);
+
+    var wallSeparateTwoH = paper.rect(448, 0, 1, 549);
+    wallSeparateTwoH.attr("fill", "SaddleBrown");
+    wallSeparateTwoH.attr("stroke", "");
+    wallSeparateTwoH.attr("stroke-width", 0);
+
+    var wallSeparateOneH = paper.rect(450, 0, 1, 549);
+    wallSeparateOneH.attr("fill", "Sienna");
+    wallSeparateOneH.attr("stroke", "");
+    wallSeparateOneH.attr("stroke-width", 0);
+
+    var wallSeparateTwoI = paper.rect(498, 0, 1, 549);
+    wallSeparateTwoI.attr("fill", "SaddleBrown");
+    wallSeparateTwoI.attr("stroke", "");
+    wallSeparateTwoI.attr("stroke-width", 0);
+
+    var wallSeparateOneI = paper.rect(500, 0, 1, 549);
+    wallSeparateOneI.attr("fill", "Sienna");
+    wallSeparateOneI.attr("stroke", "");
+    wallSeparateOneI.attr("stroke-width", 0);
+
+    var wallSeparateTwoJ = paper.rect(548, 0, 1, 549);
+    wallSeparateTwoJ.attr("fill", "SaddleBrown");
+    wallSeparateTwoJ.attr("stroke", "");
+    wallSeparateTwoJ.attr("stroke-width", 0);
+
+    var wallSeparateOneJ = paper.rect(550, 0, 1, 549);
+    wallSeparateOneJ.attr("fill", "Sienna");
+    wallSeparateOneJ.attr("stroke", "");
+    wallSeparateOneJ.attr("stroke-width", 0);
+
+    var wallSeparateTwoK = paper.rect(598, 0, 1, 549);
+    wallSeparateTwoK.attr("fill", "SaddleBrown");
+    wallSeparateTwoK.attr("stroke", "");
+    wallSeparateTwoK.attr("stroke-width", 0);
+
+    var wallSeparateOneK = paper.rect(600, 0, 1, 549);
+    wallSeparateOneK.attr("fill", "Sienna");
+    wallSeparateOneK.attr("stroke", "");
+    wallSeparateOneK.attr("stroke-width", 0);
+
+    var wallSeparateTwoL = paper.rect(648, 0, 1, 549);
+    wallSeparateTwoL.attr("fill", "SaddleBrown");
+    wallSeparateTwoL.attr("stroke", "");
+    wallSeparateTwoL.attr("stroke-width", 0);
+
+    var wallSeparateOneL = paper.rect(650, 0, 1, 549);
+    wallSeparateOneL.attr("fill", "Sienna");
+    wallSeparateOneL.attr("stroke", "");
+    wallSeparateOneL.attr("stroke-width", 0);
+
+    var wallSeparateTwoM = paper.rect(698, 0, 1, 549);
+    wallSeparateTwoM.attr("fill", "SaddleBrown");
+    wallSeparateTwoM.attr("stroke", "");
+    wallSeparateTwoM.attr("stroke-width", 0);
+
+    var wallSeparateOneM  = paper.rect(700, 0, 1, 549);
+    wallSeparateOneM.attr("fill", "Sienna");
+    wallSeparateOneM.attr("stroke", "");
+    wallSeparateOneM.attr("stroke-width", 0);
+
+    var wallSeparateTwoN = paper.rect(698, 0, 1, 549);
+    wallSeparateTwoN.attr("fill", "SaddleBrown");
+    wallSeparateTwoN.attr("stroke", "");
+    wallSeparateTwoN.attr("stroke-width", 0);
+
+    var wallSeparateOneN = paper.rect(700, 0, 1, 549);
+    wallSeparateOneN.attr("fill", "Sienna");
+    wallSeparateOneN.attr("stroke", "");
+    wallSeparateOneN.attr("stroke-width", 0);
+
+    var wallSeparateTwoO = paper.rect(748, 0, 1, 549);
+    wallSeparateTwoO.attr("fill", "SaddleBrown");
+    wallSeparateTwoO.attr("stroke", "");
+    wallSeparateTwoO.attr("stroke-width", 0);
+
+    var wallSeparateOneO = paper.rect(750, 0, 1, 549);
+    wallSeparateOneO.attr("fill", "Sienna");
+    wallSeparateOneO.attr("stroke", "");
+    wallSeparateOneO.attr("stroke-width", 0);
+
+    var wallSeparateTwoP = paper.rect(798, 0, 1, 549);
+    wallSeparateTwoP.attr("fill", "SaddleBrown");
+    wallSeparateTwoP.attr("stroke", "");
+    wallSeparateTwoP.attr("stroke-width", 0);
+
+    var wallSeparateOneP = paper.rect(800, 0, 1, 549);
+    wallSeparateOneP.attr("fill", "Sienna");
+    wallSeparateOneP.attr("stroke", "");
+    wallSeparateOneP.attr("stroke-width", 0);
+
+    var wallSeparateTwoQ = paper.rect(848, 0, 1, 549);
+    wallSeparateTwoQ.attr("fill", "SaddleBrown");
+    wallSeparateTwoQ.attr("stroke", "");
+    wallSeparateTwoQ.attr("stroke-width", 0);
+
+    var wallSeparateOneQ = paper.rect(850, 0, 1, 549);
+    wallSeparateOneQ.attr("fill", "Sienna");
+    wallSeparateOneQ.attr("stroke", "");
+    wallSeparateOneQ.attr("stroke-width", 0);
+
+    var wallSeparateTwoR = paper.rect(898, 0, 1, 549);
+    wallSeparateTwoR.attr("fill", "SaddleBrown");
+    wallSeparateTwoR.attr("stroke", "");
+    wallSeparateTwoR.attr("stroke-width", 0);
+
+    var wallSeparateOneR = paper.rect(900, 0, 1, 549);
+    wallSeparateOneR.attr("fill", "Sienna");
+    wallSeparateOneR.attr("stroke", "");
+    wallSeparateOneR.attr("stroke-width", 0);
+
+    var wallSeparateTwoS = paper.rect(948, 0, 1, 549);
+    wallSeparateTwoS.attr("fill", "SaddleBrown");
+    wallSeparateTwoS.attr("stroke", "");
+    wallSeparateTwoS.attr("stroke-width", 0);
+
+    var wallSeparateOneS = paper.rect(950, 0, 1, 549);
+    wallSeparateOneS.attr("fill", "Sienna");
+    wallSeparateOneS.attr("stroke", "");
+    wallSeparateOneS.attr("stroke-width", 0);
+
     var telerikSeenFromLeft = paper.image("images/telerik-left.png", 100, 100, 250, 210);
     var telerikSeenFromRight = paper.image("images/telerik-right.png", 650, 100, 250, 210);
 
@@ -136,5 +336,5 @@
 
     var tvSet = paper.image("images/tv.png", 350, 360, 300, 190);
     var tvAnimation = paper.image("images/animation.gif", 368, 377, 265, 133);
-    tvAnimation.attr({ opacity: 0.3 });
+    //tvAnimation.attr({ opacity: 0.3 });
 }
